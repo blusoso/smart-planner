6567fba76ac036aebfc88d125de5639a5a4ccf68
bluso <fahstjlps@gmail.com>
2022-09-27 21:29:14 +0700

    adjust global style

--- components/Input/Input.styled.ts
+++ components/Input/Input.styled.ts
@@ -12,25 +12,25 @@ export const InputWrapper = styled.div<IconPositionType>`
   svg {
     position: absolute;
     width: 24px;
-    fill: ${({ theme }) => theme.textSecondaryColor};
+    fill: ${({ theme }) => theme.grayColor};
     top: 50%;
     transform: translate(0, -50%);
-    ${({ isStartIcon }) => isStartIcon && "left: 1.5em;"}
-    ${({ isEndIcon }) => isEndIcon && "right: 1.5%;"}
+    ${({ isStartIcon }) => isStartIcon && "left: 1.3em;"}
+    ${({ isEndIcon }) => isEndIcon && "right: 1.3em;"}
   }
 `;
 
 export const InputStyled = styled.input<IconPositionType>`
   width: 100%;
-  border: 1px solid ${({ theme }) => theme.grayColor};
+  border: 1px solid ${({ theme }) => theme.lightGrayColor};
   border-radius: ${({ theme }) => theme.borderRadius};
-  padding: 1.5em ${({ isEndIcon }) => (isEndIcon ? "4em" : "1.8em")} 1.2em
-    ${({ isStartIcon }) => (isStartIcon ? "4em" : "1.8em")};
+  padding: 1em ${({ isEndIcon }) => (isEndIcon ? "3.5em" : "1.8em")} 1em
+    ${({ isStartIcon }) => (isStartIcon ? "3.5em" : "1.8em")};
   box-sizing: border-box;
   font-weight: 500;
 
   &::placeholder {
-    color: ${({ theme }) => theme.textSecondaryColor};
+    color: ${({ theme }) => theme.grayColor};
     font-weight: 400;
     font-family: "Montserrat", sans-serif;
   }
--- styles/theme.ts
+++ styles/theme.ts
@@ -1,7 +1,12 @@
 const mainTheme = {
-  textSecondaryColor: "#AEB5BF",
-  grayColor: "#e4e8ee",
+  lightGrayColor: "#e4e8ee",
+  grayColor: "#AEB5BF",
+  LightGreenColor: "#F5FAF8",
+  greenColor: "#21BA71",
+  overlayGreenColor: "rgba(33, 186, 113, 70%)",
+
   borderRadius: "1.8em",
+  mainBorderRadius: "1.56em",
 };
 
 export const theme = {
